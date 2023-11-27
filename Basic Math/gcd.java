c35f812131851b0efc7b8a39eddde0551b0b1adb
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-11-27 16:39:51 +0530

    Update gcd.java

--- Basic Math/gcd.java
+++ Basic Math/gcd.java
@@ -1,18 +1,18 @@
-public class Main
-{
-public static int gcd(int a, int b) {
-    if (b == 0) {
-        return a;
-    } else {
-        return gcd(b, a % b);
+public class GCDExample {
+    public static void main(String[] args) {
+        int num1 = 48;
+        int num2 = 18;
+
+        int result = gcd(num1, num2);
+
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + result);
     }
-}
-
-
-  
-	public static void main(String[] args) {
-        int x=6;
-        int y=8;
-		System.out.println(gcd(x,y));
-	}
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 }
