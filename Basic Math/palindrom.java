a6325e146b3100d712a5f7146b0d088b9bf5a26d
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-11-27 16:33:35 +0530

    Create palindrom.java

---
+++ Basic Math/palindrom.java
@@ -0,0 +1,19 @@
+public class Main
+{
+public static boolean isPalindrome(int num) {
+    int original = num;
+    int reversed = 0;
+    while (num > 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+    return original == reversed;
+}
+
+  
+	public static void main(String[] args) {
+        int x=5;
+		System.out.println(isPalindrome(x));
+	}
+}
