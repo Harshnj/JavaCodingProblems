7a1f16f9905ab6299c62a7818ee7421bdaeef9ed
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-11-27 16:40:57 +0530

    Update Factorial.java

--- Basic Math/Factorial.java
+++ Basic Math/Factorial.java
@@ -1,15 +1,21 @@
-public class Main
-{
-  static int factorial(int n) {
-    if (n == 1) 
-        return 1;
-    else 
-        return n * factorial(n - 1);
-    
-}
-  
-	public static void main(String[] args) {
-        int x=5;
-		System.out.println(factorial(x));
-	}
+public class FactorialExample {
+    public static void main(String[] args) {
+        int number = 5;
+
+        int result = factorial(number);
+
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+
+    public static int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+        }
+
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
