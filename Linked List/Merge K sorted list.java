178e3ad2e7f117880a6140ded6413bfadb2ea566
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-12-04 14:44:28 +0530

    Create Merge K sorted list.java

---
+++ Linked List/Merge K sorted list.java
@@ -0,0 +1,43 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return mergeLists(lists, 0, lists.length - 1);
+    }
+
+    private ListNode mergeLists(ListNode[] lists, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return lists[start];
+        }
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeLists(lists, start, mid);
+        ListNode right = mergeLists(lists, mid + 1, end);
+        return mergeTwoLists(left, right);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null) {
+            current.next = l1;
+        }
+        if (l2 != null) {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
